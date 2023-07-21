3e2cc4d0899f210d7d08850a7b44f4ecf3ce4d71
phptechlife <112804372+phptechlife@users.noreply.github.com>
2023-07-22 00:13:29 +0530

    Update laravel-part-25-snippet.js

--- laravel-part-25-snippet.js
+++ laravel-part-25-snippet.js
@@ -1,17 +1,15 @@
 $('.add').click(function(){
-      var id = $(this).data('id');
-      if (parseInt($(this).parent().prev().val()) < 10) {
-          $(this).parent().prev().val(parseInt($(this).parent().prev().val())+1);
-          var qty = $(this).parent().prev().val();
-          updateCart(id,qty)
+      var qtyElement = $(this).parent().prev(); // Qty Input
+      var qtyValue = parseInt(qtyElement.val());
+      if (qtyValue < 10) {
+          qtyElement.val(qtyValue+1);
       }            
   });
 
   $('.sub').click(function(){
-      var id = $(this).data('id');
-      if (parseInt($(this).parent().next().val()) > 1) {
-          $(this).parent().next().val(parseInt($(this).parent().next().val())-1);
-          var qty = $(this).parent().next().val();
-          updateCart(id,qty)
-      }            
-  }); 
+      var qtyElement = $(this).parent().next(); 
+      var qtyValue = parseInt(qtyElement.val());
+      if (qtyValue > 1) {
+          qtyElement.val(qtyValue-1);
+      }        
+  });
