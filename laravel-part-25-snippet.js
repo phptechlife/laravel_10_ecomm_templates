1c321c37d4e351fea053b408dd3f990b6fef9bd6
phptechlife <112804372+phptechlife@users.noreply.github.com>
2023-07-22 00:46:07 +0530

    Update laravel-part-25-snippet.js

--- laravel-part-25-snippet.js
+++ laravel-part-25-snippet.js
@@ -13,3 +13,8 @@ $('.add').click(function(){
           qtyElement.val(qtyValue-1);
       }        
   });
+
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
